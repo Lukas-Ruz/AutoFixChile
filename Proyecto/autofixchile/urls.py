c96ec14b2f61867c2745c9a8b9e5065c49cdcaed
Lukas-Ruz <luk.ruz@duocuc.cl>
2025-09-22 23:47:28 -0300

    Delete Proyecto directory

--- Proyecto/autofixchile/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import Inicio, Contratacion, Contacto, Servicios
-
-urlpatterns = [
-    path('Inicio', Inicio, name='Inicio'),
-    path('Inicio/Contratacion', Contratacion, name= 'Contratacion'),
-    path('Inicio/Contacto', Contacto, name= 'Contacto'),
-    path('Inicio/Servicios', Servicios, name= 'Servicios')
-]
