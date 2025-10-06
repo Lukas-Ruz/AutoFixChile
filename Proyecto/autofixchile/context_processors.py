e637d0eb3ba4d1f35ccfaa5ea63544f461699001
Lukas-Ruz <luk.ruz@duocuc.cl>
2025-10-06 17:35:30 -0300

    Delete Proyecto directory

--- Proyecto/autofixchile/context_processors.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth import get_user_model
-
-def user_context(request):
-
-    User = get_user_model() 
-    return {
-        'user': request.user if request.user.is_authenticated else None,
-    }
